3fe1fdfefab6177c8ae6d777d18745771eb88488
HeroBuxx <me@herobuxx.me>
2025-08-16 02:02:49 +0800

    Settings: Features: Create QS Panel Settings
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ src/com/android/settings/halcyon/display/QuickSettingsPreferences.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019-2021 ConquerOS Project
+ *           (C) 2021-2025 Halcyon Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.halcyon.display;
+
+import android.os.Bundle;
+import android.content.Context;
+
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class QuickSettingsPreferences extends DashboardFragment {
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return "QuickSettingsPreferences";
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.quicksettings_perferences;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HALCYON;
+    }
+}

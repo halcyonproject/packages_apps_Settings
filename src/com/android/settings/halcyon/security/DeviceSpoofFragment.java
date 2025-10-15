ed00ce3fce744a0bd31230b8595f9b401139f4b6
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-15 16:00:52 +0800

    Settings: Features: Allow user to provide keybox data [2/2]
    Change-Id: I6c079a9dfeb23136cc98f57f15dc101ec9f47e9f
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ src/com/android/settings/halcyon/security/DeviceSpoofFragment.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2019-2021 ConquerOS Project
+ *           (C) 2021-2025 Halcyon Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.halcyon.security;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.preference.Preference;
+
+import com.android.settings.applications.KeyboxDataPreference;
+import com.android.settings.dashboard.DashboardFragment;
+
+import com.android.settings.R;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class DeviceSpoofFragment extends DashboardFragment {
+
+    private Context mContext;
+    
+    private static final String KEYBOX_DATA_KEY = "keybox_data_setting";
+    private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
+    private KeyboxDataPreference mKeyboxDataPreference;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mKeyboxFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    Preference pref = findPreference(KEYBOX_DATA_KEY);
+                    if (pref instanceof KeyboxDataPreference) {
+                        ((KeyboxDataPreference) pref).handleFileSelected(uri);
+                    }
+                }
+            }
+        );
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mKeyboxDataPreference = findPreference(KEYBOX_DATA_KEY);
+        if (mKeyboxDataPreference != null) {
+            mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
+        }
+    }
+
+    @Override
+    protected String getLogTag() {
+        return "DeviceSpoofFragment";
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.device_spoof_preferences;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.HALCYON;
+    }
+}

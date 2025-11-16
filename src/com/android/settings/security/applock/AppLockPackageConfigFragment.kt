CLO-Ext/android_packages_apps_Settings

Settings: Introduce App Lock [3/3]
Squashed:

* AppLockCredentialActivity is a stripped down version of ConfirmDeviceCredentialActivity in Settings

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

Settings: applock: adapt for API changes

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

Settings: use a new task stack for app lock fragments

* fixes fragments staying in recents on going home
* also made other preferences do binder calls asynchronously

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

Settings: applock: fix unlock prompt disappearing when trying to open apps

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

Settings: applock: add hidden app settings

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

AppLockPackageConfigFragment: allow launching app from app icon

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

FlamingoSettings: applock: early return in setChecked if backing field has the same value

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

[nift4: drop useless plurals.xml, make timeout values translatable]

* Dhina17 <dhinalogu@gmail.com>
        applock: Adapt for Android 14

neobuddy89:
Adapted for A15.

Change-Id: I85d72ee72353417ead528483bbbe1ac1e6860063
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/security/applock/AppLockPackageConfigFragment.kt
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.internal.logging.nano.MetricsProto
+
+import com.android.settings.R
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.widget.EntityHeaderController
+import com.android.settingslib.applications.ApplicationsState.AppEntry
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.widget.LayoutPreference
+
+private val TAG = AppLockPackageConfigFragment::class.simpleName
+private const val KEY_HEADER = "header_view"
+
+class AppLockPackageConfigFragment : DashboardFragment() {
+
+    private lateinit var packageInfo: PackageInfo
+
+    override fun onAttach(context: Context) {
+        packageInfo = arguments?.getParcelable(PACKAGE_INFO, PackageInfo::class.java)!!
+        super.onAttach(context)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        val appEntry = AppEntry(requireContext(), packageInfo.applicationInfo, 0)
+        val header = preferenceScreen.findPreference<LayoutPreference>(KEY_HEADER)
+        EntityHeaderController.newInstance(
+            requireActivity(),
+            this,
+            header?.findViewById(R.id.entity_header)
+        ).setRecyclerView(listView, settingsLifecycle)
+            .setPackageName(packageInfo.packageName)
+            .setButtonActions(
+                EntityHeaderController.ActionType.ACTION_NONE,
+                EntityHeaderController.ActionType.ACTION_NONE
+            )
+            .bindHeaderButtons()
+            .setLabel(appEntry)
+            .setIcon(appEntry)
+            .done(requireActivity(), false /* rebindActions */)
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockPackageProtectionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockNotificationRedactionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockHideAppPC(context, packageInfo.packageName, lifecycleScope)
+    )
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.NEOTERIC
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_config_settings
+
+    override protected fun getLogTag() = TAG
+}

src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -1,0 +1,143 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
+import android.os.Bundle
+import android.view.View
+
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
+import androidx.preference.forEach
+
+import com.android.internal.logging.nano.MetricsProto
+
+import com.android.settings.R
+import com.android.settings.core.SubSettingLauncher
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settingslib.PrimarySwitchPreference
+import com.android.settingslib.widget.TwoTargetPreference.ICON_SIZE_SMALL
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private val TAG = AppLockPackageListFragment::class.simpleName
+internal const val PACKAGE_INFO = "package_info"
+
+class AppLockPackageListFragment : DashboardFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private lateinit var pm: PackageManager
+    private lateinit var whiteListedPackages: Array<String>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        appLockManager = context.getSystemService(AppLockManager::class.java)
+        pm = context.packageManager
+        whiteListedPackages = resources.getStringArray(
+            com.android.internal.R.array.config_appLockAllowedSystemApps)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        lifecycleScope.launch {
+            val selectedPackages = getSelectedPackages()
+            val preferences = withContext(Dispatchers.Default) {
+                pm.getInstalledPackages(
+                    PackageInfoFlags.of(PackageManager.MATCH_ALL.toLong())
+                ).filter {
+                    !it.applicationInfo.isSystemApp() || whiteListedPackages.contains(it.packageName)
+                }.sortedWith { first, second ->
+                    getLabel(first).compareTo(getLabel(second))
+                }
+            }.map { packageInfo ->
+                createPreference(packageInfo, selectedPackages.contains(packageInfo.packageName))
+            }
+            preferenceScreen?.let {
+                preferences.forEach { pref ->
+                    it.addPreference(pref)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            val selectedPackages = getSelectedPackages()
+            preferenceScreen?.forEach {
+                if (it is PrimarySwitchPreference) {
+                    it.isChecked = selectedPackages.contains(it.key)
+                }
+            }
+        }
+    }
+
+    private suspend fun getSelectedPackages(): Set<String> {
+        return withContext(Dispatchers.IO) {
+            appLockManager.packageData.map { it.packageName }.toSet()
+        }
+    }
+
+    private fun getLabel(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadLabel(pm).toString()
+
+    private fun createPreference(packageInfo: PackageInfo, isProtected: Boolean): Preference {
+        val label = getLabel(packageInfo)
+        return PrimarySwitchPreference(requireContext()).apply {
+            key = packageInfo.packageName
+            title = label
+            icon = packageInfo.applicationInfo.loadIcon(pm)
+            setIconSize(ICON_SIZE_SMALL)
+            isChecked = isProtected
+            setOnPreferenceChangeListener { _, newValue ->
+                lifecycleScope.launch(Dispatchers.IO) {
+                    if (newValue as Boolean) {
+                        appLockManager.addPackage(packageInfo.packageName)
+                    } else {
+                        appLockManager.removePackage(packageInfo.packageName)
+                    }
+                }
+                return@setOnPreferenceChangeListener true
+            }
+            setOnPreferenceClickListener {
+                SubSettingLauncher(requireContext())
+                    .setDestination(AppLockPackageConfigFragment::class.qualifiedName)
+                    .setSourceMetricsCategory(metricsCategory)
+                    .setTitleText(label)
+                    .setArguments(
+                        Bundle(1).apply {
+                            putParcelable(PACKAGE_INFO, packageInfo)
+                        }
+                    )
+                    .launch()
+                true
+            }
+        }
+    }
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.NEOTERIC
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_list_settings
+
+    override protected fun getLogTag() = TAG
+}

src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.os.SystemProperties
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+import com.android.internal.logging.nano.MetricsProto
+import androidx.lifecycle.lifecycleScope
+
+import com.android.settings.R
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.search.SearchIndexable
+
+@SearchIndexable
+class AppLockSettingsFragment : DashboardFragment(),
+    MenuItem.OnMenuItemClickListener {
+
+    private var debugEnabled = SystemProperties.get(DEBUG_PROPERTY, null) == LEVEL_DEBUG
+    private var handledClick = false
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override fun getMetricsCategory() = MetricsProto.MetricsEvent.NEOTERIC
+
+    override protected fun getLogTag() = TAG
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+        menu.add(
+            0 /* groupId */,
+            MENU_ITEM_DEBUG_ID,
+            0 /* order */,
+            getDebugMenuItemTitle(),
+        ).setOnMenuItemClickListener(this)
+    }
+
+    private fun getDebugMenuItemTitle(): Int =
+        if (debugEnabled) R.string.disable_debugging else R.string.enable_debugging
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        if (item.itemId == MENU_ITEM_DEBUG_ID) {
+            debugEnabled = !debugEnabled
+            SystemProperties.set(DEBUG_PROPERTY, if (debugEnabled) LEVEL_DEBUG else null)
+            item.setTitle(getDebugMenuItemTitle())
+            return true
+        }
+        return false
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockBiometricPreferenceController(context, lifecycleScope)
+    )
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        private const val DEBUG_PROPERTY = "log.tag.AppLockManagerService"
+        private const val LEVEL_DEBUG = "DEBUG"
+        private const val MENU_ITEM_DEBUG_ID = 101
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.app_lock_settings)
+    }
+}

halcyonproject/packages_apps_Settings

Signed-off-by: HeroBuxx <me@herobuxx.me>

src/com/android/settings/security/applock/AppLockPackageConfigFragment.kt
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+
+import androidx.lifecycle.lifecycleScope
+
+import com.android.internal.logging.nano.MetricsProto
+
+import com.android.settings.R
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.widget.EntityHeaderController
+import com.android.settingslib.applications.ApplicationsState.AppEntry
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.widget.LayoutPreference
+
+private val TAG = AppLockPackageConfigFragment::class.simpleName
+private const val KEY_HEADER = "header_view"
+
+class AppLockPackageConfigFragment : DashboardFragment() {
+
+    private lateinit var packageInfo: PackageInfo
+
+    override fun onAttach(context: Context) {
+        packageInfo = arguments?.getParcelable(PACKAGE_INFO, PackageInfo::class.java)!!
+        super.onAttach(context)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        val appEntry = AppEntry(requireContext(), packageInfo.applicationInfo, 0)
+        val header = preferenceScreen.findPreference<LayoutPreference>(KEY_HEADER)
+        EntityHeaderController.newInstance(
+            requireActivity(),
+            this,
+            header?.findViewById(R.id.entity_header)
+        ).setRecyclerView(listView, settingsLifecycle)
+            .setPackageName(packageInfo.packageName)
+            .setButtonActions(
+                EntityHeaderController.ActionType.ACTION_NONE,
+                EntityHeaderController.ActionType.ACTION_NONE
+            )
+            .bindHeaderButtons()
+            .setLabel(appEntry)
+            .setIcon(appEntry)
+            .done(requireActivity(), false /* rebindActions */)
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockPackageProtectionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockNotificationRedactionPC(context, packageInfo.packageName, lifecycleScope),
+        AppLockHideAppPC(context, packageInfo.packageName, lifecycleScope)
+    )
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.HALCYON
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_config_settings
+
+    override protected fun getLogTag() = TAG
+}

src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -1,0 +1,143 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.app.AppLockManager
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
+import android.os.Bundle
+import android.view.View
+
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.Preference
+import androidx.preference.forEach
+
+import com.android.internal.logging.nano.MetricsProto
+
+import com.android.settings.R
+import com.android.settings.core.SubSettingLauncher
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settingslib.PrimarySwitchPreference
+import com.android.settingslib.widget.TwoTargetPreference.ICON_SIZE_SMALL
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+private val TAG = AppLockPackageListFragment::class.simpleName
+internal const val PACKAGE_INFO = "package_info"
+
+class AppLockPackageListFragment : DashboardFragment() {
+
+    private lateinit var appLockManager: AppLockManager
+    private lateinit var pm: PackageManager
+    private lateinit var whiteListedPackages: Array<String>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        appLockManager = context.getSystemService(AppLockManager::class.java)
+        pm = context.packageManager
+        whiteListedPackages = resources.getStringArray(
+            com.android.internal.R.array.config_appLockAllowedSystemApps)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        lifecycleScope.launch {
+            val selectedPackages = getSelectedPackages()
+            val preferences = withContext(Dispatchers.Default) {
+                pm.getInstalledPackages(
+                    PackageInfoFlags.of(PackageManager.MATCH_ALL.toLong())
+                ).filter {
+                    !it.applicationInfo.isSystemApp() || whiteListedPackages.contains(it.packageName)
+                }.sortedWith { first, second ->
+                    getLabel(first).compareTo(getLabel(second))
+                }
+            }.map { packageInfo ->
+                createPreference(packageInfo, selectedPackages.contains(packageInfo.packageName))
+            }
+            preferenceScreen?.let {
+                preferences.forEach { pref ->
+                    it.addPreference(pref)
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            val selectedPackages = getSelectedPackages()
+            preferenceScreen?.forEach {
+                if (it is PrimarySwitchPreference) {
+                    it.isChecked = selectedPackages.contains(it.key)
+                }
+            }
+        }
+    }
+
+    private suspend fun getSelectedPackages(): Set<String> {
+        return withContext(Dispatchers.IO) {
+            appLockManager.packageData.map { it.packageName }.toSet()
+        }
+    }
+
+    private fun getLabel(packageInfo: PackageInfo) =
+        packageInfo.applicationInfo.loadLabel(pm).toString()
+
+    private fun createPreference(packageInfo: PackageInfo, isProtected: Boolean): Preference {
+        val label = getLabel(packageInfo)
+        return PrimarySwitchPreference(requireContext()).apply {
+            key = packageInfo.packageName
+            title = label
+            icon = packageInfo.applicationInfo.loadIcon(pm)
+            setIconSize(ICON_SIZE_SMALL)
+            isChecked = isProtected
+            setOnPreferenceChangeListener { _, newValue ->
+                lifecycleScope.launch(Dispatchers.IO) {
+                    if (newValue as Boolean) {
+                        appLockManager.addPackage(packageInfo.packageName)
+                    } else {
+                        appLockManager.removePackage(packageInfo.packageName)
+                    }
+                }
+                return@setOnPreferenceChangeListener true
+            }
+            setOnPreferenceClickListener {
+                SubSettingLauncher(requireContext())
+                    .setDestination(AppLockPackageConfigFragment::class.qualifiedName)
+                    .setSourceMetricsCategory(metricsCategory)
+                    .setTitleText(label)
+                    .setArguments(
+                        Bundle(1).apply {
+                            putParcelable(PACKAGE_INFO, packageInfo)
+                        }
+                    )
+                    .launch()
+                true
+            }
+        }
+    }
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.HALCYON
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_list_settings
+
+    override protected fun getLogTag() = TAG
+}

src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2022 FlamingoOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.applock
+
+import android.content.Context
+import android.os.SystemProperties
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+import com.android.internal.logging.nano.MetricsProto
+import androidx.lifecycle.lifecycleScope
+
+import com.android.settings.R
+import com.android.settings.dashboard.DashboardFragment
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.core.AbstractPreferenceController
+import com.android.settingslib.search.SearchIndexable
+
+@SearchIndexable
+class AppLockSettingsFragment : DashboardFragment(),
+    MenuItem.OnMenuItemClickListener {
+
+    private var debugEnabled = SystemProperties.get(DEBUG_PROPERTY, null) == LEVEL_DEBUG
+    private var handledClick = false
+
+    override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
+
+    override fun getMetricsCategory() = MetricsProto.MetricsEvent.HALCYON
+
+    override protected fun getLogTag() = TAG
+
+    override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, menuInflater)
+        menu.add(
+            0 /* groupId */,
+            MENU_ITEM_DEBUG_ID,
+            0 /* order */,
+            getDebugMenuItemTitle(),
+        ).setOnMenuItemClickListener(this)
+    }
+
+    private fun getDebugMenuItemTitle(): Int =
+        if (debugEnabled) R.string.disable_debugging else R.string.enable_debugging
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        if (item.itemId == MENU_ITEM_DEBUG_ID) {
+            debugEnabled = !debugEnabled
+            SystemProperties.set(DEBUG_PROPERTY, if (debugEnabled) LEVEL_DEBUG else null)
+            item.setTitle(getDebugMenuItemTitle())
+            return true
+        }
+        return false
+    }
+
+    override protected fun createPreferenceControllers(
+        context: Context
+    ) : List<AbstractPreferenceController> = listOf(
+        AppLockBiometricPreferenceController(context, lifecycleScope)
+    )
+
+    companion object {
+        private const val TAG = "AppLockSettingsFragment"
+
+        private const val DEBUG_PROPERTY = "log.tag.AppLockManagerService"
+        private const val LEVEL_DEBUG = "DEBUG"
+        private const val MENU_ITEM_DEBUG_ID = 101
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.app_lock_settings)
+    }
+}